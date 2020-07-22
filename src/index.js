757c0e6d333fd1bb88e2bc6aa204d6db867bbbc1
Marcos Velasco <devslife7@gmail.com>
2020-07-21 23:39:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const taskForm = document.getElementById('create-task-form')
+    const taskUl = document.getElementById('tasks')
+    const taskInput = document.getElementById('new-task-description')
+    const taskList = document.getElementById('list')
+
+    taskForm.addEventListener('submit', e => {
+        e.preventDefault()
+
+        const inputText = e.target['new-task-description'].value // grab the text from the input field
+        const listLi = document.createElement('li') // creates new <li> element
+        listLi.textContent = inputText // adds the input value to our new li element
+        taskUl.append(listLi) // appends our new li to existing
+        taskInput.value = ''
+
+        const btn = document.createElement('button')
+        btn.textContent = 'X'
+        listLi.append(btn)
+
+        btn.addEventListener('click', e => {
+            e.target.parentElement.remove()
+        })
+    })
+})
